e3b6c0baa5e292f7e07eec56532fc342c993d469
travisneto <travisneto@live.com>
2024-09-28 20:01:35 -0400

    Finished

--- src/components/IngredientList.jsx
+++ src/components/IngredientList.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-
-// src/components/IngredientList.jsx
-const IngredientList = ({availableIngredients, addToBurger}) => {
-    return (<ul>
-       {availableIngredients.map((ingredient, index) => {
-        <li key={index} style={{ color: ingredient.color}}>
-            {ingredient.name}
-            <button onClick={() => addToBurger(ingredient)}>+</button>
-        </li>
-       })}
-    </ul>
+const IngredientList = (props) => {
+    return (
+        <ul>
+            {props.ingredients.map((ingredient, index) => (
+                <li key={index} style={{ backgroundColor: ingredient.color }}>
+                    {ingredient.name}
+                    <button
+                    onClick={props.addToBurger}
+                    name={ingredient.name}
+                    value={ingredient.color}
+                >
+                    +
+                </button>
+                </li>
+            ))}
+        </ul>
     )
-  }
-  
-  export default IngredientList
+}
+
+export default IngredientList;
   
